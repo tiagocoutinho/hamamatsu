f2ac584cd8af188c8c7add1f5354b3f3476e8312
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-02-03 22:33:21 +0100

    Update URL in setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://gitlab.com/tiagocoutinho/hamamatsu',
+    url='https://github.com/tiagocoutinho/hamamatsu',
     version='0.1.1',
     zip_safe=False,
     python_requires=">=3.7",
