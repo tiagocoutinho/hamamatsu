11ac51f13fae842ee1dee22d7413d174d83cb2b0
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-09-02 14:47:55 +0200

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/tiagocoutinho/hamamatsu',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
     python_requires=">=3.7",
     entry_points={
